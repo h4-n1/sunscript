bd057c1df5985fc7ae5b87a005929f49da4fa30a
h4n1 <>
2021-05-11 16:40:12 -0400

    more cleanup

--- sunscript.py
+++ sunscript.py
@@ -6,26 +6,23 @@ import pytz
 from astral import LocationInfo
 from astral.sun import dawn, sunrise, sunset, dusk
 
-# define location
+### ALL USER INPUT HERE
+# define location and date range
 loc_name = 'Toronto'
+# what's the point of the region definition?
 loc_region = 'Ontario'
 loc_lat = 43.651070
 loc_long = -79.347015
-loc_tz = 'US/Eastern'
+# is there a better way to define timezone?
+loc_tz = 'America/Toronto'
 date_start = date(2021, 1, 1)
 # daterange function is exclusive of end date
 date_end = date(2022, 1, 1)
+output_filename = 'sun_{0}_{1}_{2}.ics'.format(loc_name.lower(), date_start, date_end)
 
 # set location
 loc = LocationInfo(loc_name, loc_region, loc_tz, loc_lat, loc_long)
 #>timezone – The location’s time zone (a list of time zone names can be obtained from pytz.all_timezones)
-
-### ASTRAL TESTS
-#print (dawn(loc.observer, testdate, tzinfo=testloc_tz))
-#print (sunrise(loc.observer, testdate, tzinfo=testloc_tz))
-# print (sunset(loc.observer, testdate, tzinfo=testloc_tz))
-# print (dusk(loc.observer, testdate, tzinfo=testloc_tz))
-# this works ^
 
 ### ICAL STUFF
 
@@ -51,36 +48,38 @@ def daterange(start_date, end_date):
 
 for loc_date in daterange(date_start, date_end):
 
-# info vars
+    ### time vars
+    
     dawntime = dawn(loc.observer, loc_date, tzinfo=loc_tz)
     risetime = sunrise(loc.observer, loc_date, tzinfo=loc_tz)
     settime = sunset(loc.observer, loc_date, tzinfo=loc_tz)
     dusktime = dusk(loc.observer, loc_date, tzinfo=loc_tz)
 
-    # description vars
-    locstring = vText('{0}, {1}, {2}'.format(loc_name, loc_lat, loc_long))
+    ### description vars
+    
+    # could move coordinates to 'GEO' property, see https://www.kanzaki.com/docs/ical/geo.html
+    locstring = vText('{0} / {1}, {2}'.format(loc_name, loc_lat, loc_long))
+    
     # timedelta doesn't allow strftime, find a way to format it better, see https://stackoverflow.com/questions/538666/format-timedelta-to-string
     risedesc = 'Dawn at {0}, sunrise at {1}. Total sunlight time {2}'.format(dawntime.strftime("%H:%M"), risetime.strftime("%H:%M"), str(settime - risetime))
 
     setdesc = 'Sunset at {0}, dusk at {1}. Total sunlight time {2}'.format(settime.strftime("%H:%M"), dusktime.strftime("%H:%M"), str(settime - risetime))
 
-
     # dawn to sunrise
     daystart = Event()
-
     daystart.add('summary', '↑ {0}'.format(risetime.strftime("%H:%M")))
-    daystart['uid'] = '{0}/SUNSCRIPT/RISE'.format(loc_date)
+    daystart['uid'] = '{0}/SUNSCRIPT/SUNRISE/{1}'.format(loc_date, loc_name.upper())
     daystart.add('dtstamp', datetime.now(timezone.utc))
     daystart['location'] = locstring
     daystart['description'] = risedesc
     daystart.add('dtstart', dawntime)
     daystart.add('dtend', risetime)
-    #>add the event to the calendar
     cal.add_component(daystart)
 
+    # sunset to dusk
     dayend = Event()
     dayend.add('summary', '↓ {0}'.format(settime.strftime("%H:%M")))
-    dayend['uid'] = '{0}/SUNSCRIPT/SET'.format(loc_date)
+    dayend['uid'] = '{0}/SUNSCRIPT/SUNSET/{1}'.format(loc_date, loc_name.upper())
     dayend.add('dtstamp', datetime.now(timezone.utc))
     dayend['location'] = locstring
     dayend['description'] = setdesc
@@ -88,15 +87,9 @@ for loc_date in daterange(date_start, date_end):
     dayend.add('dtend', dusktime)
     cal.add_component(dayend)
 
-### FOR LOOP SHOULD END HERE
-
-# just for debugging
-# print("cal is ", cal)
-
 # write to disk
 import os
-# change to .ics
-f=open('test.ics', 'wb')
+f=open(output_filename, 'wb')
 f.write(cal.to_ical())
 
 
