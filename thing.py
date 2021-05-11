3313137d060d78de28f6d9916bf773698d4155ae
h4n1 <>
2021-05-11 13:51:02 -0400

    changed variable names to remove 'test'

--- thing.py
+++ thing.py
@@ -8,13 +8,13 @@ from astral import LocationInfo
 from astral.sun import dawn, sunrise, sunset, dusk
 
 # define test location
-testloc_lat = 43.651070
-testloc_long = -79.347015
-testloc_tz = 'US/Eastern'
-testdate=date(2021,5,9)
+loc_lat = 43.651070
+loc_long = -79.347015
+loc_tz = 'US/Eastern'
+loc_date=date(2021,5,9)
 
 # set location
-loc = LocationInfo("name", "region", testloc_tz, testloc_lat, testloc_long)
+loc = LocationInfo("name", "region", loc_tz, loc_lat, loc_long)
 #>timezone – The location’s time zone (a list of time zone names can be obtained from pytz.all_timezones)
 
 ### ASTRAL TESTS
@@ -29,6 +29,8 @@ loc = LocationInfo("name", "region", testloc_tz, testloc_lat, testloc_long)
 # https://icalendar.readthedocs.io/en/latest/usage.html#example
 from icalendar import Calendar, Event, vDatetime
 
+# maybe change the whole thing to use variables instead of calculating within the function, if it's going to be duplicated as strings in event descriptions anyway
+
 #>init the calendar
 cal = Calendar()
 #>compliance properties
@@ -36,22 +38,22 @@ cal.add('prodid', '-//sunscript//h4n1//')
 cal.add('version', '0.1')
 
 # info vars
-risetime = sunrise(loc.observer, testdate, tzinfo=testloc_tz).strftime("%H:%M:%S")
-settime = sunset(loc.observer, testdate, tzinfo=testloc_tz).strftime("%H:%M:%S")
+risetime = sunrise(loc.observer, loc_date, tzinfo=loc_tz).strftime("%H:%M:%S")
+settime = sunset(loc.observer, loc_date, tzinfo=loc_tz).strftime("%H:%M:%S")
 
 # dawn to sunrise
 daystart = Event()
 daystart.add('summary', '↑ {0}'.format(risetime))
-daystart.add('dtstart', dawn(loc.observer, testdate, tzinfo=testloc_tz))
-daystart.add('dtend', sunrise(loc.observer, testdate, tzinfo=testloc_tz))
+daystart.add('dtstart', dawn(loc.observer, loc_date, tzinfo=loc_tz))
+daystart.add('dtend', sunrise(loc.observer, loc_date, tzinfo=loc_tz))
 
 #>add the event to the calendar
 cal.add_component(daystart)
 
 dayend = Event()
 dayend.add('summary', '↓ {0}'.format(settime))
-dayend.add('dtstart', sunset(loc.observer, testdate, tzinfo=testloc_tz))
-dayend.add('dtend', dusk(loc.observer, testdate, tzinfo=testloc_tz))
+dayend.add('dtstart', sunset(loc.observer, loc_date, tzinfo=loc_tz))
+dayend.add('dtend', dusk(loc.observer, loc_date, tzinfo=loc_tz))
 cal.add_component(dayend)
 
 print("cal is ", cal)
