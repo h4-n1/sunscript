0c7d16f310759785ad7e4fae8687c9c2e51bb44c
h4n1 <>
2021-05-11 15:33:43 -0400

    this actually seems to work

--- thing.py
+++ thing.py
@@ -12,7 +12,9 @@ loc_region = 'Ontario'
 loc_lat = 43.651070
 loc_long = -79.347015
 loc_tz = 'US/Eastern'
-loc_date=date(2021,5,9)
+date_start = date(2021, 1, 1)
+# daterange function is exclusive of end date
+date_end = date(2022, 1, 1)
 
 # set location
 loc = LocationInfo(loc_name, loc_region, loc_tz, loc_lat, loc_long)
@@ -41,43 +43,50 @@ cal.add('prodid', '-//sunscript//h4n1//')
 cal.add('version', '2.0')
 
 ### FOR LOOP SHOULD START HERE
+# from https://stackoverflow.com/a/1060330
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+# could possibly also use dateutil module
+
+for loc_date in daterange(date_start, date_end):
 
 # info vars
-dawntime = dawn(loc.observer, loc_date, tzinfo=loc_tz)
-risetime = sunrise(loc.observer, loc_date, tzinfo=loc_tz)
-settime = sunset(loc.observer, loc_date, tzinfo=loc_tz)
-dusktime = dusk(loc.observer, loc_date, tzinfo=loc_tz)
+    dawntime = dawn(loc.observer, loc_date, tzinfo=loc_tz)
+    risetime = sunrise(loc.observer, loc_date, tzinfo=loc_tz)
+    settime = sunset(loc.observer, loc_date, tzinfo=loc_tz)
+    dusktime = dusk(loc.observer, loc_date, tzinfo=loc_tz)
 
-# description vars
-locstring = vText('{0}, {1}, {2}'.format(loc_name, loc_lat, loc_long))
-# timedelta doesn't allow strftime, find a way to format it better, see https://stackoverflow.com/questions/538666/format-timedelta-to-string
-risedesc = 'Dawn at {0}, sunrise at {1}. Total sunlight time {2}'.format(dawntime.strftime("%H:%M"), risetime.strftime("%H:%M"), str(settime - risetime))
+    # description vars
+    locstring = vText('{0}, {1}, {2}'.format(loc_name, loc_lat, loc_long))
+    # timedelta doesn't allow strftime, find a way to format it better, see https://stackoverflow.com/questions/538666/format-timedelta-to-string
+    risedesc = 'Dawn at {0}, sunrise at {1}. Total sunlight time {2}'.format(dawntime.strftime("%H:%M"), risetime.strftime("%H:%M"), str(settime - risetime))
 
-setdesc = 'Sunset at {0}, dusk at {1}. Total sunlight time {2}'.format(settime.strftime("%H:%M"), dusktime.strftime("%H:%M"), str(settime - risetime))
+    setdesc = 'Sunset at {0}, dusk at {1}. Total sunlight time {2}'.format(settime.strftime("%H:%M"), dusktime.strftime("%H:%M"), str(settime - risetime))
 
 
-# dawn to sunrise
-daystart = Event()
+    # dawn to sunrise
+    daystart = Event()
 
-daystart.add('summary', '↑ {0}'.format(risetime.strftime("%H:%M")))
-daystart['uid'] = '{0}/SUNSCRIPT/RISE'.format(loc_date)
-daystart.add('dtstamp', datetime.now(timezone.utc))
-daystart['location'] = locstring
-daystart['description'] = risedesc
-daystart.add('dtstart', dawntime)
-daystart.add('dtend', risetime)
-#>add the event to the calendar
-cal.add_component(daystart)
+    daystart.add('summary', '↑ {0}'.format(risetime.strftime("%H:%M")))
+    daystart['uid'] = '{0}/SUNSCRIPT/RISE'.format(loc_date)
+    daystart.add('dtstamp', datetime.now(timezone.utc))
+    daystart['location'] = locstring
+    daystart['description'] = risedesc
+    daystart.add('dtstart', dawntime)
+    daystart.add('dtend', risetime)
+    #>add the event to the calendar
+    cal.add_component(daystart)
 
-dayend = Event()
-dayend.add('summary', '↓ {0}'.format(settime.strftime("%H:%M")))
-dayend['uid'] = '{0}/SUNSCRIPT/SET'.format(loc_date)
-dayend.add('dtstamp', datetime.now(timezone.utc))
-dayend['location'] = locstring
-dayend['description'] = setdesc
-dayend.add('dtstart', settime)
-dayend.add('dtend', dusktime)
-cal.add_component(dayend)
+    dayend = Event()
+    dayend.add('summary', '↓ {0}'.format(settime.strftime("%H:%M")))
+    dayend['uid'] = '{0}/SUNSCRIPT/SET'.format(loc_date)
+    dayend.add('dtstamp', datetime.now(timezone.utc))
+    dayend['location'] = locstring
+    dayend['description'] = setdesc
+    dayend.add('dtstart', settime)
+    dayend.add('dtend', dusktime)
+    cal.add_component(dayend)
 
 ### FOR LOOP SHOULD END HERE
 
